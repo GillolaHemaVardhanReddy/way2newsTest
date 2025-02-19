e0370407b9ba2afd4ad1cb457efd8b70e95db9b7
GillolaHemaVardhanReddy <gillolahemavardhanreddy@gmail.com>
2025-02-19 14:19:08 +0530

    solved warnings

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Outlet, useNavigate } from 'react-router-dom';
 import welcomeposter from '../../assets/welcomeposter.jpg'
 
 const WelcomePageme = () => {
-    const navigate = useNavigate();
   return (
     <div className='welcome-image'>
         <img alt='welcome poster' src={welcomeposter}/>
